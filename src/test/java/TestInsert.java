09fea60d86fa45cfbf22367e5d15098bd19bb903
mayas <mayas.ait-elhadj@etu.univ-amu.fr>
2021-05-19 00:02:32 +0200

    Quelques corrections !

--- src/test/java/TestInsert.java
+++ src/test/java/TestInsert.java
@@ -1,12 +1,6 @@
-import connectionManager.PasswordAuthentication;
-import connectionManager.Session;
-
-
 public class TestInsert {
 
     public static void main(String[] args) {
-
-        Session newSession = new Session("Mayas", "supermayas123");
 
     }
 }
