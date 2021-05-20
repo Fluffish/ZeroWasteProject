c61cea2073df202877a3daef3ed56f395557f34e
mayas <mayas.ait-elhadj@etu.univ-amu.fr>
2021-05-20 20:11:10 +0200

    Implémentation des méthodes permettant de gérer les recettes.

--- src/main/java/model/UserUtilities.java
+++ src/main/java/model/UserUtilities.java
@@ -4,9 +4,7 @@ import model.Hibernate.Tables.Food;
 import model.Hibernate.Tables.Recipe;
 import model.Hibernate.Tables.User_Possess;
 import model.Hibernate.Tables.Users;
-import model.Hibernate.TablesManager.FoodManager;
-import model.Hibernate.TablesManager.UserPossessManager;
-import model.Hibernate.TablesManager.UsersManager;
+import model.Hibernate.TablesManager.*;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -17,6 +15,9 @@ public class UserUtilities {
     private static final UsersManager USERS_MANAGER = new UsersManager();
     private static final UserPossessManager USER_POSSESS_MANAGER = new UserPossessManager();
     private static final FoodManager FOOD_MANAGER = new FoodManager();
+    private static final RecipeManager RECIPE_MANAGER = new RecipeManager();
+    private static final RecipeTypeManager RECIPE_TYPE_MANAGER = new RecipeTypeManager();
+    private static final UseFoodManager USE_FOOD_MANAGER = new UseFoodManager();
     private static final Food EMPTY_STORAGE = new Food(1, "null");
 
     public static void addUserStorage(Users user, int idStorage) {
@@ -27,8 +28,8 @@ public class UserUtilities {
     public static void addUserPossess(Users user, int idStorage, int idFood, String quantity,
                                       Timestamp expirationDate, float price) {
         User_Possess newUserPosses = new User_Possess(user.getId_user(), idStorage, idFood,
-                                                      quantity, Timestamp.valueOf(LocalDateTime.now()),
-                                                      expirationDate, price);
+                                                      quantity, expirationDate,
+                                                      Timestamp.valueOf(LocalDateTime.now()), price);
 
         USER_POSSESS_MANAGER.Insert(User_Possess.class.getName(), newUserPosses);
 
@@ -44,7 +45,7 @@ public class UserUtilities {
 
         for (int id_storage : userStorages){
             Storage storage = new Storage(id_storage);
-            List<Integer>foods = USER_POSSESS_MANAGER.FindFood(id_storage);
+            List<Integer> foods = USER_POSSESS_MANAGER.FindFood(id_storage);
             for (int id_food : foods){
                 if (id_food != 1)
                     storage.add(FOOD_MANAGER.FindFoodById(id_food));
@@ -72,22 +73,60 @@ public class UserUtilities {
     }
 
     public static List<Food> getVeryLimitedFood(Users user) {
-        /* TODO */
-        return null;
+        List<Food> veryLimitedFood = new ArrayList<Food>(0);
+        List<Integer> foods = USER_POSSESS_MANAGER.FindVeryLimitedFood(user.getId_user());
+
+        for (int id_food : foods){
+            if (id_food != 1)
+                veryLimitedFood.add(FOOD_MANAGER.FindFoodById(id_food));
+        }
+
+        return veryLimitedFood;
     }
 
     public static List<Food> getLimitedFood(Users user) {
-        /* TODO */
-        return null;
+        List<Food> limitedFood = new ArrayList<Food>(0);
+        List<Integer> foods = USER_POSSESS_MANAGER.FindLimitedFood(user.getId_user());
+
+        for (int id_food : foods){
+            if (id_food != 1)
+                limitedFood.add(FOOD_MANAGER.FindFoodById(id_food));
+        }
+
+        return limitedFood;
     }
 
     public static List<Food> getNotUrgentFood(Users user) {
-        /* TODO */
-        return null;
+        List<Food> nonUrgentFood = new ArrayList<Food>(0);
+        List<Integer> foods = USER_POSSESS_MANAGER.FindNotUrgentFood(user.getId_user());
+
+        for (int id_food : foods){
+            if (id_food != 1)
+                nonUrgentFood.add(FOOD_MANAGER.FindFoodById(id_food));
+        }
+
+        return nonUrgentFood;
     }
 
-    public static List<Recipe> getAvailableRecipes(Users user) {
-        /* TODO */
-        return null;
+    public static List<Recipe> getAvailableRecipes(Users user, StorageRoom storageRoom) {
+        List<Recipe> availableRecipes = new ArrayList<>(0);
+        List<Recipe> recipes = RECIPE_MANAGER.GetAllRecipe();
+
+        for (Recipe recipe : recipes) {
+            boolean isAvailable = true;
+            List<Integer> ingredients = USE_FOOD_MANAGER.FindUsedFood(recipe.getId_recipe());
+
+            for (Integer ingrendient : ingredients) {
+                if (!storageRoom.containsFood(ingrendient)) {
+                    isAvailable = false;
+                    break;
+                }
+            }
+
+            if (isAvailable)
+                availableRecipes.add(recipe);
+        }
+
+        return availableRecipes;
     }
 }
