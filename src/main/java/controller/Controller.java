d81685b18e32d5c68b4aa170c9d8e06b2908ffb7
Fluffish <56067847+Fluffish@users.noreply.github.com>
2021-05-22 18:35:35 +0200

    Correction des erreurs dues aux merges.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,6 +1,5 @@
 package controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
@@ -8,14 +7,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuButton;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
-import java.awt.*;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -79,54 +73,10 @@ public class Controller implements Initializable {
     @FXML
     public void SignUpButtonAction(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent homepage = FXMLLoader.load(getClass().getResource("/homepage2.fxml"));
-
-    @FXML
-    public void StorageRedirection(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent homepage = FXMLLoader.load(getClass().getResource("/Stockage.fxml"));
-        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(homepage);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-    @FXML
-    public void MyFoodRediction(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent homepage = FXMLLoader.load(getClass().getResource("/Food.fxml"));
-        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(homepage);
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-    @FXML
-    public void AvailableDishes(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent homepage = FXMLLoader.load(getClass().getResource("/dish.fxml"));
-        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(homepage);
-        stage.setScene(scene);
-        stage.show();
-
-
-    }
-
-    @FXML
-    public void MyProfil(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent homepage = FXMLLoader.load(getClass().getResource("/profil.fxml"));
-        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(homepage);
-        stage.setScene(scene);
-        stage.show();
-
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    }
-
-
-    public void AddToTheList(javafx.event.ActionEvent actionEvent) {
     }
 }
--- src/test/java/TestInsert.java
+++ src/test/java/TestInsert.java
@@ -6,23 +6,6 @@ import model.connectionManager.SignUp;
 public class TestInsert {
 
     public static void main(String[] args) {
-//        SignUp.createUser("Djursy", "djursybis", "djursy@gmail.com", "00 00 00 00 00", 500, 425);
 
-        UsersManager usersManager = new UsersManager();
-        Session session = new Session("Mayas", "SuperMayas123");
-
-        session.utilities.addUserPossess(1, 2, "200g", AppUtils.getTimestampForOneWeekAway(), 5f);
-
-        System.out.println(session.utilities.getAllFood());
-
-        session.disconnect();
-
-        session = new Session("Djursy", "djursybis");
-
-        System.out.println(session.utilities.getAllFood());
-
-        session.disconnect();
-
-        System.out.println(session.isConnected());
     }
 }
